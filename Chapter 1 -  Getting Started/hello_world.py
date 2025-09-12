149f85d9c1f45fd81103ae5c02efa45b69f80b6b
Jeynicks <jeynicks7@gmail.com>
2025-09-12 22:11:09 +0800

    Chapter 2

--- Chapter 1 -  Getting Started/hello_world.py
+++ Chapter 1 -  Getting Started/hello_world.py
@@ -5,7 +5,7 @@
 #1-2. Hello World Typos: Open the hello_world.py file you just created. Make a
 #typo somewhere in the line and run the program again. Can you make a typo
 #that generates an error? Can you make sense of the error message? 
-Hello World("print")
+print("Hello World!")
 
 #Can you
 #make a typo that doesn’t generate an error? Why do you think it didn’t make
